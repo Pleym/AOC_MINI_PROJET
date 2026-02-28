111926422f5517ad1770447570f225cea602efee
Pleym <abauldorian@gmail.com>
2026-02-28 09:17:00 +0100

    correct the sh

--- NPB3.4.4/NPB3.4-MPI/script_benchmark.sh
+++ NPB3.4.4/NPB3.4-MPI/script_benchmark.sh
@@ -7,7 +7,7 @@
 #SBATCH --mem=10G
 #SBATCH --nodes=1
 #SBATCH --constraint=x64cpu
-#SBATCH --ntasks=1
+#SBATCH --ntasks=16
 #SBATCH --cpus-per-task=1
 #SBATCH --error=ft.%J.err
 #SBATCH --output=ft.%J.out
@@ -17,6 +17,7 @@ romeo_load_x64cpu_env
 spack load ^openmpi@4.1.7  %aocc
 
 make clean
+mkdir -p bin
 make ft CLASS=C
 
-mpirun -np 16 ./bin/ft.C.x
+mpirun -np "${SLURM_NTASKS}" ./bin/ft.C.x
