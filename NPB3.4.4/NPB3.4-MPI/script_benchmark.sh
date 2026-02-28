4b292b1c68b7a6e8f65769a05dccc675d7d5f602
Pleym <abauldorian@gmail.com>
2026-02-28 09:39:46 +0100

    correct script

--- NPB3.4.4/NPB3.4-MPI/script_benchmark.sh
+++ NPB3.4.4/NPB3.4-MPI/script_benchmark.sh
@@ -2,6 +2,7 @@
 
 
 
+
 #SBATCH --account="r250142"
 #SBATCH --time=10:00
 #SBATCH --mem=10G
@@ -14,14 +15,26 @@
 
 romeo_load_x64cpu_env
 
-spack load ^openmpi@4.1.7  %aocc
-spack load ^maqao@2025.1.4 %aocc
+spack load openmpi@4.1.7%aocc || spack load openmpi || true
+spack load maqao@2025.1.4%aocc || spack load maqao || true
 
 make clean
 mkdir -p bin
 make ft CLASS=C
 
-maqao oneview -R1 xp=maqao_oneview_xp_ft_C -- mpirun -np "${SLURM_NTASKS}" ./bin/ft.C.x
+MPI_LAUNCHER=""
+if command -v mpirun >/dev/null 2>&1; then
+	MPI_LAUNCHER="mpirun -np ${SLURM_NTASKS}"
+elif command -v mpiexec >/dev/null 2>&1; then
+	MPI_LAUNCHER="mpiexec -n ${SLURM_NTASKS}"
+elif command -v srun >/dev/null 2>&1; then
+	MPI_LAUNCHER="srun -n ${SLURM_NTASKS}"
+else
+	echo "Erreur: aucun lanceur MPI trouvé (mpirun/mpiexec/srun)."
+	exit 1
+fi
+
+maqao oneview -R1 xp=maqao_oneview_xp_ft_C --mpi-command="${MPI_LAUNCHER}" -- ./bin/ft.C.x
 
 # mpirun -np "${SLURM_NTASKS}" ./bin/ft.C.x
 
